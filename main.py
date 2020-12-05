b62890e7ee75a8b9c956b26469e5cdcca533f04c
AppiGarg <appigarg@hotmail.com>
2020-12-05 22:15:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+PhoneBook = {"Arpit": {"Number": 777777, "Email": "FakeEmail.com", "Webpage": "Arpit.com"}, "David": {"Number": 888888, "Email": "FakeEmail.com", "Webpage": "David.com"}}
+
+print(PhoneBook["Arpit"]["Email"])
+print(PhoneBook["David"]["Webpage"])
+
+Name = input("Who do you Seek?")
+Request = input("What do you Seek?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
